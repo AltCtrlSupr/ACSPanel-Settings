63d841067515493880b7dbafdd75aa975d16739c
Genar Trias Ortiz <genar@acs.li>
2015-12-04 18:32:41 +0100

    Adding new ServiceSettingType for adding settings to entity

---
+++ Form/ServiceSettingType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ACS\ACSPanelSettingsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use ACS\ACSPanelSettingsBundle\Form\EventListener\AdaptFormSubscriber;
+
+class ServiceSettingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $user_fields = $this->user_fields;
+
+        $builder
+            ->add('setting_key','hidden')
+            ->add('context', 'hidden')
+            ->add('focus', 'hidden')
+        ;
+
+        $subscriber = new AdaptFormSubscriber($builder->getFormFactory(), $user_fields);
+        $builder->addEventSubscriber($subscriber);
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ACS\ACSPanelBundle\Entity\PanelSetting',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'acs_settingsbundle_configsettingtype';
+    }
+}
